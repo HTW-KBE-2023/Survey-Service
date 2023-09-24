85a32f9f2ea6f96d8ac755551aa98228f56eb5de
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2023-09-24 19:17:58 +0200

    K2-30 Add Autoinclude of Navigationproperties

--- Survey-API/Port/Database/SurveyContext.cs
+++ Survey-API/Port/Database/SurveyContext.cs
@@ -2,6 +2,7 @@
 using API.Models.SurveyOptions;
 using API.Models.Surveys;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace API.Port.Database
 {
@@ -14,5 +15,16 @@ namespace API.Port.Database
         public DbSet<Survey> Surveys { get; set; }
         public DbSet<SurveyOption> SurveyOptions { get; set; }
         public DbSet<Participant> Participants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Survey>()
+                        .Navigation(survey => survey.SurveyOptions)
+                        .AutoInclude();
+
+            modelBuilder.Entity<Survey>()
+                        .Navigation(survey => survey.Participants)
+                        .AutoInclude();
+        }
     }
 }
