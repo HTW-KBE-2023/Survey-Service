a833586166f17a48bbd1cb19f9e3f03727b229da
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2023-09-25 16:18:26 +0200

    Give Validator a concrete Message for SurveyOptions

--- Survey-API/Models/Surveys/SurveyValidator.cs
+++ Survey-API/Models/Surveys/SurveyValidator.cs
@@ -9,7 +9,7 @@ public class SurveyValidator : AbstractValidator<Survey>
         RuleFor(survey => survey.Id).NotEmpty();
         RuleFor(survey => survey.Title).NotEmpty();
         RuleFor(survey => survey.Description).NotNull();
-        RuleFor(survey => survey.SurveyOptions).Must(surveyOptions => surveyOptions.Count >= 2 && surveyOptions.Count <= 8);
+        RuleFor(survey => survey.SurveyOptions).Must(surveyOptions => surveyOptions.Count >= 2 && surveyOptions.Count <= 8).WithMessage("The Survey must have between 2 and 8 Options (Inklusiv))");
         RuleFor(survey => survey.Participants).NotNull();
     }
 }
